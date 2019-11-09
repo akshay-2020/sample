c09695bc1c61882a8caf222af08aaa8a140b06be
akshay-2020 <ab11g19@soton.ac.uk>
2019-11-09 00:53:28 +0000

    first commit

---
+++ file/example.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+void method1() 
+{
+
+}
+
+int main(void)
+{
+	int x,y,z;
+	//printf("Hello world!\n");
+	//scanf("%i",&x);
+	//printf("%i\n",x);
+	//pow(2,3);
+	
+	for (x=0; x < 10; x++)
+	{
+		printf("hi\n");
+	}
+}
